312e4131040dbc4010fed2bd9d26db49ee442edb
Sush1sui <jplovesraw@gmail.com>
2024-11-25 22:25:22 +0800

    rsadfada

--- src/events/memberBoost.ts
+++ src/events/memberBoost.ts
@@ -26,7 +26,7 @@ export default {
           .setTitle("Thank you for the server boost!")
           .setColor(0xff73fa) // Customize the embed color
           .setDescription(
-            `${newMember} We truly appreciate your support and all you do to help make this community even better! Sending you all our love and gratitude!\n\n` +
+            `**${newMember} We truly appreciate your support and all you do to help make this community even better! Sending you all our love and gratitude!**\n\n` +
               "> **Perks**\n" +
               "- Receive <@&1292420325002448930> role\n" +
               "- Custom Onigiri Color Role <#1303919788342382615>\n" +
